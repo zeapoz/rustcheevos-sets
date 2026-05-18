be701f06d7c846013900fb201fe9c73fc4f87f7d
zeapoz <zeapo@pm.me>
2026-05-18 23:25:56 +0900

    feat: add rustcheevos embebddable cli

--- crates/nds/geometry-wars-galaxies/src/main.rs
+++ crates/nds/geometry-wars-galaxies/src/main.rs
@@ -1,8 +1,9 @@
-use std::env;
+use std::io;
 
 use leaderboards::generate_leaderboards;
 use rich::generate_rich_presence;
 use rustcheevos::types::game::GameData;
+use rustcheevos_cli::RustcheevosCli;
 use set::generate_set;
 
 mod leaderboards;
@@ -13,9 +14,7 @@ mod types;
 const GAME_ID: u32 = 20374;
 const GAME_NAME: &str = "Geometry Wars: Galaxies";
 
-const OUTPUT_DIR_NAME: &str = "output";
-
-fn main() {
+fn main() -> io::Result<()> {
     let mut game_data = GameData::new(GAME_ID, GAME_NAME);
 
     game_data
@@ -23,10 +22,5 @@ fn main() {
         .set_leaderboards(generate_leaderboards())
         .set_rich_presence(generate_rich_presence());
 
-    let output_dir = env::current_dir()
-        .expect("failed to get current directory")
-        .join(OUTPUT_DIR_NAME);
-    game_data
-        .export(output_dir)
-        .expect("failed to export game assets");
+    RustcheevosCli::parse().run(&game_data)
 }
