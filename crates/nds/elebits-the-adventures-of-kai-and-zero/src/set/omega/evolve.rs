67bab0949ceb0bf4999029bc2c256c50c7c3898f
zeapoz <zeapo@pm.me>
2026-09-01 08:31:05 +0200

    Update evolve.rs

--- crates/nds/elebits-the-adventures-of-kai-and-zero/src/set/omega/evolve.rs
+++ crates/nds/elebits-the-adventures-of-kai-and-zero/src/set/omega/evolve.rs
@@ -43,19 +43,23 @@ fn evolve_omega_achivement() -> Achievement {
 
 fn evolve_half_achivement() -> Achievement {
     let num_evolvable = Omega::all_evolvable().len();
+    let half_num_evolvable = (num_evolvable / 2) as u32;
+
     let all_evolvable: ResolvedChain = Omega::all_evolvable()[..num_evolvable - 1]
         .into_iter()
         .map(|o| add_source!(bit1!(o.obtained_addr())))
         .collect();
-    let alt_groups: Vec<_> = Omega::all_evolvable()
-        .into_iter()
-        .map(|o| delta!(o.obtained_state().eq(OmegaState::Obtained as u32)))
-        .collect();
+    let all_evolvable_delta: ResolvedChain = all_evolvable.iter().map(|r| r.delta()).collect();
+
     let core = chain!(
+        all_evolvable_delta,
+        delta!(
+            bit1!(Omega::all_evolvable().last().unwrap().obtained_addr())
+                .eq(half_num_evolvable - 1)
+        ),
         all_evolvable,
         measured!(
-            bit1!(Omega::all_evolvable().last().unwrap().obtained_addr())
-                .eq((num_evolvable / 2) as u32)
+            bit1!(Omega::all_evolvable().last().unwrap().obtained_addr()).eq(half_num_evolvable)
         ),
         measured_if!(mem::game_state().eq(GameState::Overworld.id())),
         Game::in_game(),
@@ -64,7 +68,6 @@ fn evolve_half_achivement() -> Achievement {
     Achievement::builder("Omega Conduit")
         .description("Evolve 12 Omegas into their adult forms")
         .core(core)
-        .alt_groups(alt_groups)
         .points(10)
         .id(626337)
         .badge_id(712171)
